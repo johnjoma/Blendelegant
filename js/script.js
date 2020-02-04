6430d22fffd3269dfd8b818115f9b5e603a71bae
johnjoma <irungujohnkimani@gmail.com>
2020-02-05 01:03:36 +0300

    BlendElegant

--- js/script.js
+++ js/script.js
@@ -1,10 +1,9 @@
-
 //SMOOTH PAGE SCROLL
-$(function() {
-  $('a[href*=#]:not([href=#])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+$(function () {
+  $('a[href*=#]:not([href=#])').click(function () {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
       var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
       if (target.length) {
         $('html,body').animate({
           scrollTop: target.offset().top
@@ -18,34 +17,34 @@ $(function() {
 
 //OWL CAROSEL TESTIMONIAL
 $('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:false,
-    dots:true,
-    dotsEach:true,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:1
-        },
-        1000:{
-            items:1
-        }
+  loop: true,
+  margin: 10,
+  nav: false,
+  dots: true,
+  dotsEach: true,
+  responsive: {
+    0: {
+      items: 1
+    },
+    600: {
+      items: 1
+    },
+    1000: {
+      items: 1
     }
+  }
 })
 
 $(document).ready(
 
-  function() { 
+  function () {
 
     $("html").niceScroll({
-    	cursorcolor:"#f74d65",
-    	scrollspeed :"100",
-    	cursorborder:"1px solid #f74d65",
-    	horizrailenabled: "false",
-    	cursorborderradius: "0px"
+      cursorcolor: "#3d3d3d",
+      scrollspeed: "100",
+      cursorborder: "1.5px solid #3d3d3d",
+      horizrailenabled: "false",
+      cursorborderradius: "0px"
     });
 
   }
@@ -55,14 +54,14 @@ $(document).ready(
 new WOW().init();
 
 
-    
+
 /*Preloader*/
 //<![CDATA[
-$(window).load(function() { // makes sure the whole site is loaded
+$(window).load(function () { // makes sure the whole site is loaded
   $('#status').fadeOut(); // will first fade out the loading animation
   $('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website.
-  $('body').delay(350).css({'overflow':'visible'});
+  $('body').delay(350).css({
+    'overflow': 'visible'
+  });
 })
 //]]>
-
-
